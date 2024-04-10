ad3004dc8d9b7930e1112fc69ecdaad51d62263a
GEORGEMUTALE <mutalegeorge367@gmail.com>
2024-04-10 12:21:00 +0200

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,23 @@
-use time::PrimitiveDateTime as DateTime;
+use time::{PrimitiveDateTime as DateTime, Date, Time, Month};
 
-// Returns a DateTime one billion seconds after start.
-pub fn after(start: DateTime) -> DateTime {
-    unimplemented!("What time is a gigasecond later than {start}");
+pub fn after( start: DateTime) -> DateTime{
+
+    let gigasecond_duration = time::Duration::seconds(1_000_000_000);
+
+    let result = start + gigasecond_duration;
+
+    result
+}
+
+fn main(){
+    let start_date = Date::from_calendar_date(2015, Month::January, 24).expect("Invalid date");
+
+    let start_time = Time::from_hms(22, 00, 00).expect("Invalid time");
+
+    let start = DateTime::new(start_date, start_time);
+
+    let result = after(start);
+
+    println!("One gigasecond after {} is {}", start, result)
+
 }
